ca35388d0a5491fe132e4a92651cf1e3ac374c1c
Vijayganesh3107 <64351388+Vijayganesh3107@users.noreply.github.com>
2020-10-11 14:02:26 +0530

    Chnages

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,11 +2,13 @@ import React from "react";
 import Square from "./Square";
 
 const Board = ({ squares, onClick }) => (
-  <div className="board">
-    {squares.map((square, i) => (
-      <Square key={i} value={square} onClick={() => onClick(i)} />
-    ))}
-  </div>
+    <div className="board">
+        {squares.map((square,index)=>{
+            return(
+                <Square key={index} value={square} onClick={()=>onClick(index)}></Square>
+            )
+        })}
+    </div>
 );
 
 export default Board;
