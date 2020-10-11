f104013574d56abade6300c592bf638a532b0020
Vijayganesh3107 <64351388+Vijayganesh3107@users.noreply.github.com>
2020-10-11 13:51:05 +0530

    CHanged the game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,52 +1,76 @@
-import React, { Fragment, useState } from "react";
-import { calculateWinner } from "../helper";
+import React, { Fragment, useState,useEffect } from "react";
 import Board from "./Board";
 
+ function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
 const Game = () => {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [boardSquares, setBoardSquares] = useState( /* localStorage.getItem("isboardfilled")?[localStorage.getItem("isboardfilled")]: */[Array(9).fill(null)]);
   const [stepNumber, setStepNumber] = useState(0);
-  const [xIsNext, setXisNext] = useState(true);
-  const winner = calculateWinner(history[stepNumber]);
-  const xO = xIsNext ? "X" : "O";
+  const [xNext, setXNext] = useState(true);
+  const winner = calculateWinner(boardSquares[stepNumber]);
+  const xO = xNext ? "X" : "O";
+
+//   useEffect(()=>{
+//       localStorage.setItem("isboardfilled",boardSquares);
+//   },[boardSquares])
 
   const handleClick = (i) => {
-    const historyPoint = history.slice(0, stepNumber + 1);
+    const historyPoint = boardSquares.slice(0, stepNumber + 1);
     const current = historyPoint[stepNumber];
     const squares = [...current];
-    // return if won or occupied
+   
     if (winner || squares[i]) return;
-    // select square
+    
     squares[i] = xO;
-    setHistory([...historyPoint, squares]);
-    setStepNumber(historyPoint.length);
-    setXisNext(!xIsNext);
+    setBoardSquares([...historyPoint, squares]);
+    setStepNumber(/* historyPoint.length */ stepNumber+1);
+    setXNext(!xNext);
   };
 
   const jumpTo = (step) => {
     setStepNumber(step);
-    setXisNext(step % 2 === 0);
+    setXNext(step % 2 === 0?true:false);
   };
 
-  const renderMoves = () =>
-    history.map((_step, move) => {
-      const destination = move ? `Go to move #${move}` : "Go to Start";
+  const trackMoves = () =>
+    boardSquares.map((_step, index) => {
+      const destination = index ? `Go to move #${index}` : "Reset";
       return (
-        <li key={move}>
-          <button onClick={() => jumpTo(move)}>{destination}</button>
+        <li key={index}>
+          <button onClick={() => jumpTo(index)}>{destination}</button>
         </li>
       );
     });
 
+    const style=winner?`winner blink_me`:``;
   return (
     <Fragment>
-      <h1>React Tic Tac Toe - With Hooks</h1>
-      <Board squares={history[stepNumber]} onClick={handleClick} />
-      <div className="info-wrapper">
+      <h1>Tic Tac Toe</h1>
+      <Board squares={boardSquares[stepNumber]} onClick={handleClick} />
+      <div className="wrapper">
         <div>
-          <h3>History</h3>
-          {renderMoves()}
+          <h3>Progress</h3>
+          {trackMoves()}
         </div>
-        <h3>{winner ? "Winner: " + winner : "Next Player: " + xO}</h3>
+        <h3 className={style}>{winner ? "Winner: " + winner : "Next Player: " + xO}</h3>
       </div>
     </Fragment>
   );
